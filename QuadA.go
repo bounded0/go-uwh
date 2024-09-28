321fb00e96b2138ad98f83bedf9892e1b06a9f15
mahmut eren ünal <82273753+bounded0@users.noreply.github.com>
2024-09-28 16:35:44 +0300

    Update QuadA.go

--- QuadA.go
+++ QuadA.go
@@ -9,16 +9,13 @@ func QuadA(x, y int) {
 	for i := 1; i <= y; i++ {
 		for j := 1; j <= x; j++ {
 			if (i == 1 || i == y) && (j == 1 || j == x) {
-				// Print 'o' at the corners
 				print("o")
 			} else if i == 1 || i == y {
-				// Print '-' at the top and bottom borders
 				print("-")
 			} else if j == 1 || j == x {
-				// Print '|' at the left and right borders
 				print("|")
 			} else if i != 1 && i != y && j != 1 && j != x {
-				// Print space inside the rectangle
+				
 				print(" ")
 			}
 
